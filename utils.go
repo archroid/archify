a274c90b6000f1023dfd03497e72988afe027573
achroid <archroid.dev@gmail.com>
2024-01-14 13:36:27 +0330

    make main page more responsive; updated shutdown,sleep and reboot functions for all os's.

--- utils.go
+++ utils.go
@@ -1,16 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"os/exec"
+	"runtime"
 
 	log "github.com/charmbracelet/log"
 )
 
-//Functions
-
 func shutdown() error {
-	cmd := exec.Command("shutdown", "-h", "now")
+
+	var cmd *exec.Cmd
+	switch runtime.GOOS {
+	case "windows":
+		cmd = exec.Command("shutdown", "/s")
+	case "linux":
+		cmd = exec.Command("shutdown", "-h", "now")
+	case "darwin":
+		cmd = exec.Command("shutdown", "-h", "now")
+	default:
+		return fmt.Errorf("unsupported platform")
+	}
+
 	err := cmd.Run()
 	if err != nil {
 		log.Error("Error shutting down:", err)
@@ -23,8 +35,18 @@ func shutdown() error {
 }
 
 func reboot() error {
-	// Execute the reboot command
-	cmd := exec.Command("reboot")
+	var cmd *exec.Cmd
+	switch runtime.GOOS {
+	case "windows":
+		cmd = exec.Command("shutdown", "/r")
+	case "linux":
+		cmd = exec.Command("reboot")
+	case "darwin":
+		cmd = exec.Command("shutdown", "-r", "now")
+	default:
+		return fmt.Errorf("unsupported platform")
+	}
+
 	err := cmd.Run()
 	if err != nil {
 		log.Error("Error rebooting: ", err)
@@ -38,7 +60,18 @@ func reboot() error {
 }
 
 func sleep() error {
-	cmd := exec.Command("systemctl", "suspend")
+	var cmd *exec.Cmd
+	switch runtime.GOOS {
+	case "windows":
+		cmd = exec.Command("rundll32.exe", "powrprof.dll,SetSuspendState", "0,1,0")
+	case "linux":
+		cmd = exec.Command("systemctl", "suspend")
+	case "darwin":
+		cmd = exec.Command("pmset", "sleepnow")
+	default:
+		return fmt.Errorf("unsupported platform")
+	}
+
 	err := cmd.Run()
 	if err != nil {
 		log.Error("Error sleeping:", err)
