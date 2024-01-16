1b5a274e2ca251e0981951c543a045db9b82cf3a
achroid <archroid.dev@gmail.com>
2024-01-16 16:59:45 +0330

    added support for multiple file upload

--- static/uploadFile.js
+++ static/uploadFile.js
@@ -3,27 +3,31 @@ function upload() {
      // Get the file input element
      var fileInput = document.getElementById('file');
 
-     // Create a new FormData object
-     var formData = new FormData();
+     for (let i = 0; i < fileInput.files.length; i++) {
 
-     // Add the file to the FormData object
-     formData.append('myFile', fileInput.files[0]);
-     const startTime = Date.now();
+          // Create a new FormData object
+          var formData = new FormData();
 
-     // Send a POST request with the file data
-     fetch("/upload", {
-          method: 'POST',
-          body: formData
-     })
-          .then(response => response.json())
-          .then(data => {
-               const endTime = Date.now();
-               const duration = endTime - startTime;
-               alert(`${JSON.stringify(data.status)}, Took: ${duration} ms`);
+          // Add the file to the FormData object
+          formData.append('myFile', fileInput.files[i]);
+
+          // const startTime = Date.now();
+
+          // Send a POST request with the file data
+          fetch("/upload", {
+               method: 'POST',
+               body: formData
           })
-          .catch((error) => {
-               alert('Error:', error);
-          });
+               .then(response => response.json())
+               .then(data => {
+                    // const endTime = Date.now();
+                    // const duration = endTime - startTime;
+                    // alert(`${JSON.stringify(data.status)}, Took: ${duration} ms`);
+               })
+               .catch((error) => {
+                    alert('Error:', error);
+               });
+     }
 }
 document.getElementById('uploadForm').addEventListener('submit', function (e) {
      e.preventDefault();
@@ -34,7 +38,7 @@ document.getElementById('uploadForm').addEventListener('submit', function (e) {
 function itemselect(target) {
      const fileNames = Array.from(target.files).map(file => file.name);
      const allNames = fileNames.join("<br>");
-     
+
      textfile = document.getElementById("file-name");
      textfile.innerHTML = allNames;
      textfile.style.display = "block";
