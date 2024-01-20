77072d7b1c3496fb5d10cbb8a9d2878bef0fc572
achroid <archroid.dev@gmail.com>
2024-01-20 15:43:40 +0330

    working on new design for index.html

---
+++ static/sciprt.js
@@ -0,0 +1,175 @@
+function ping() {
+     const startTime = Date.now();
+     fetch(window.location.href + "/ping")
+          .then(response => response.json())
+          .then(data => {
+               const endTime = Date.now();
+               const duration = endTime - startTime;
+               alert(`${JSON.stringify(data.resp)}, Time: ${duration} ms`);
+          })
+          .catch((error) => {
+               console.error('Error:', error);
+          });
+
+}
+
+function accessServer() {
+     window.location.href = window.location.href + "/dir/";
+
+}
+
+function reboot() {
+     fetch(window.location.href + "/reboot")
+          .then(response => response.json())
+          .then(data =>
+               alert(`${JSON.stringify(data.resp)}`)
+          )
+          .catch((error) => {
+               console.error('Error:', error);
+          });
+
+}
+
+function shutdown() {
+     fetch(window.location.href + "/shutdown")
+          .then(response => response.json())
+          .then(data => console.log(data))
+          .catch((error) => {
+               console.error('Error:', error);
+          });
+}
+
+function sleep() {
+     fetch(window.location.href + "/sleep")
+          .then(response => response.json())
+          .then(data => console.log(data))
+          .catch((error) => {
+               console.error('Error:', error);
+          });
+}
+
+
+const toggler = document.getElementById('theme-toggle');
+
+toggler.addEventListener('change', function () {
+     if (this.checked) {
+          document.body.classList.add('dark');
+     } else {
+          document.body.classList.remove('dark');
+     }
+});
+
+
+function upload() {
+     // Get the file input element
+     var fileInput = document.getElementById('file');
+     var progressBar = document.getElementById('uploadProgress');
+
+     // Calculate total size of all files
+     var totalSize = 0;
+     for (let i = 0; i < fileInput.files.length; i++) {
+          totalSize += fileInput.files[i].size;
+     }
+
+     // Keep track of total uploaded size
+     var totalLoaded = 0;
+
+     for (let i = 0; i < fileInput.files.length; i++) {
+          var formData = new FormData();
+          formData.append('myFile', fileInput.files[i]);
+
+          var xhr = new XMLHttpRequest();
+
+          progressBar.style.visibility = "visible";
+
+          // Update progress bar
+          xhr.upload.onprogress = function (e) {
+               if (e.lengthComputable) {
+                    totalLoaded += e.loaded;
+                    progressBar.max = totalSize;
+                    progressBar.value = totalLoaded;
+               }
+          };
+
+          // Load end
+          xhr.onloadend = function () {
+               if (xhr.status == 200) {
+                    console.log("upload complete");
+               } else {
+                    console.log("upload failed");
+               }
+          };
+
+          xhr.open('POST', '/upload', true);
+          xhr.send(formData);
+     }
+}
+
+
+function itemselect(target) {
+     const fileNames = Array.from(target.files).map(file => file.name);
+
+     // Assuming you have a table with id "myTable"
+     var table = document.getElementById("name-table");
+     table.innerHTML = '';
+     // Loop through the array
+     for (var i = 0; i < fileNames.length; i++) {
+          // Create a new row
+          var row = table.insertRow();
+
+          // Create a new cell for the file type
+          var typeCell = row.insertCell();
+
+          // Determine the file type based on the file extension
+          var fileType = fileNames[i].split('.').pop().toLowerCase();
+          console.log(fileType);
+
+          // Choose an emoji based on the file type
+          var emoji;
+          switch (fileType) {
+               case 'mp4':
+               case 'mkv':
+               case 'avi':
+               case 'mov':
+               case 'flv':
+               case 'wmv':
+               case 'webm':
+                    emoji = '🎥';
+                    break;
+               case 'jpg':
+               case 'jpeg':
+               case 'gif':
+               case 'png':
+               case 'bmp':
+                    emoji = '📸';
+                    break;
+               case 'mp3':
+               case 'wav':
+               case 'ogg':
+               case 'flac':
+                    emoji = '🎵';
+                    break;
+               case 'zip':
+               case 'rar':
+               case '7z':
+               case 'tar':
+               case 'gz':
+               case 'bz2':
+               case 'xz':
+                    emoji = '📦';
+                    break
+               default:
+                    emoji = '📄';
+          }
+
+          // Insert the emoji into the type cell
+          typeCell.textContent = emoji;
+
+          // Create a new cell for the filename
+          var nameCell = row.insertCell();
+
+          // Insert the filename into the name cell
+          nameCell.textContent = fileNames[i];
+     }
+
+}
